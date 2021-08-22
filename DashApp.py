992d48e2fe659abbdd13aed9884dbccf501ced28
andibb <68001318+andibb@users.noreply.github.com>
2021-08-22 21:07:49 +0200

    Add files via upload

--- DashApp.py
+++ DashApp.py
@@ -5,11 +5,12 @@ from modules.Webserver import Webserver
 
 from modules.Controller import Controller
 from modules.View import View
-from modules.Model import Model
 
 import pytz
 import datetime
 import pandas as pd
+
+import textwrap
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -31,28 +32,7 @@ API_KEY = os.getenv('API_KEY')
 
 class DashApp:
     def __init__(self):
-        self.Webserver = Webserver(dash,html,dcc,go,px)
-        self.DataHandler = DataHandler(pd,relativedelta,os,pytz,datetime,API_KEY,EntsoePandasClient)
+        self.Webserver = Webserver(dash,html,dcc,go,px,pd,relativedelta,os,pytz,datetime,API_KEY,EntsoePandasClient,textwrap,Input, Output)
         print("DashApp Initiated")
 
 DashApp = DashApp()
-
-#print(DashApp.DataHandler.get_data_container(metada_grp_call))
-# start,end = DashApp.DataHandler.get_day_times()
-# metadata = {
-#     'region' : 'DE',
-#     'start' : start-relativedelta(days=0),
-#     'end'    : end-relativedelta(days=0),
-#     'data_type' : 'live',
-#     'forecast_type' : 'statistical'
-# }
-
-# print(DashApp.DataHandler.get_data(metadata))
-
-#    
-# summ_gC02_per_KWh = np.zeros(len(df))
-# for el in df.columns:
-#     summ_gC02_per_KWh += self.calc_percentage(renewable_energies=[el])/100*lookup_c02_intesities[el]
-
-
-# print(DashApp.DataHandler.get_percentage(metadata))
